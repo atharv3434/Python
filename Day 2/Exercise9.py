85ba61ed95854a1d05a8bff775a836357b8d25bb
atharv3434 <atharvanakti008@gmail.com>
2026-09-01 23:49:23 +0530

    Added some new Files

---
+++ Day 2/Exercise9.py
@@ -0,0 +1,20 @@
+"""
+
+Exercise 9: Longest Palindromic Substring
+Write a program that prompts the user to enter a text string and finds the longest substring within it that reads the same forward and backward. 
+If there are multiple palindromic substrings of the same maximum length, print any one of them.
+
+Sample Input: "babad"
+Sample Output: "bab" (or "aba")
+Sample Input: "cbbd"
+Sample Output: "bb"
+
+"""
+
+def main():
+    text = input("Enter a Text : ")
+    start = text(0)
+    print(start)
+
+
+
